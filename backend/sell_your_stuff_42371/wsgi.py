01242e88aaa4f102675f14fff0664deacf098831
Crowdbotics <team@crowdbotics.com>
2023-07-23 20:25:41 +0000

    Initial commit.

---
+++ backend/sell_your_stuff_42371/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sell_your_stuff_42371 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sell_your_stuff_42371.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sell-your-stuff-42371",
+  url: "https://sell-your-stuff-42371.botics.co",
+  api: "https://sell-your-stuff-42371.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
